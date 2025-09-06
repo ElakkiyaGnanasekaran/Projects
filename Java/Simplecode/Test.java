5c3bc2caac5590986b825597234b0edfed19df10
Elakkiya <gelakkiya2000@gmail.com>
2025-09-06 17:55:30 +0530

    Create Test.java

---
+++ Java/Simplecode/Test.java
@@ -0,0 +1,12 @@
+package Java.Simplecode;
+
+public class Test {
+    public static void main(String arg[]){
+        int a = 15;
+        int b = 20;
+        int c = sum(a,b);
+        System.out.println(c);
+        
+    }
+    
+}
