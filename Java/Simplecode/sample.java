effd02847768c926fdde708ab4b3de147deebcc0
Elakkiya <gelakkiya2000@gmail.com>
2025-09-06 19:11:31 +0530

    Create sample.java

---
+++ Java/Simplecode/sample.java
@@ -0,0 +1,17 @@
+package Java.Simplecode;
+import java.util.Scanner;
+
+public class sample{
+    public static void main(String arg[]){
+        int a = 15;
+        int b = 20;
+        if(a>b){
+            System.out.println("a is greater than b");
+        }
+        else{
+            System.out.println("b is greater than a");
+        }
+    }
+}
+    
+       
