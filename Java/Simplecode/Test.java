f62b408c4931e488962bd169e68d788590f53592
Elakkiya <gelakkiya2000@gmail.com>
2025-09-06 18:04:12 +0530

    Update Test.java

--- Java/Simplecode/Test.java
+++ Java/Simplecode/Test.java
@@ -4,9 +4,11 @@ public class Test {
     public static void main(String arg[]){
         int a = 15;
         int b = 20;
-        int c = sum(a,b);
+        float c = summary(a,b);
         System.out.println(c);
-        
     }
-    
+
+    private static float summary(int x, int y) {
+        return x - y;
+    }
 }
