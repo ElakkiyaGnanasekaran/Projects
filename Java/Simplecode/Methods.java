00eccd3e2dd7c1699e3559081d60e7050b9d67e9
Elakkiya <gelakkiya2000@gmail.com>
2025-09-07 21:16:17 +0530

    Create Methods.java

---
+++ Java/Simplecode/Methods.java
@@ -0,0 +1,12 @@
+package Java.Simplecode;
+
+public class Methods {
+    public void main(String args[]){
+    add(10,13);
+    System.out.println("a is printed");
+}
+public static int add(int a, int b){
+    int c = a+b;
+    return c;
+}
+}
