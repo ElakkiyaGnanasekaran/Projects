cb9d476068e59e8c28e8f5e5d9c6782d45660cb0
Elakkiya <gelakkiya2000@gmail.com>
2025-09-07 10:08:04 +0530

    Create switchExample.java

---
+++ Java/Simplecode/switchExample.java
@@ -0,0 +1,37 @@
+package Java.Simplecode;
+
+public class switchExample {
+    public static void main(String args[]){
+        int days =3;
+        switch (days){
+            case 1:
+              System.out.println("Sunday");
+              break;
+            case 2:
+               System.out.println("Monday");
+               break;
+            case 3:
+                 System.out.println("tuesday");
+                 break;
+            case 4:
+                  System.out.println("Wednesday");
+                  break;
+            case 5:
+                  System.out.println("thursday");
+                  break;
+            case 6:
+                  System.out.println("Friday");
+                  break;
+            case 7:
+                 System.out.println("Staturday");
+                 break;
+            default:
+                 System.out.println("Invalid days");
+                 
+
+
+
+        }
+    }
+    
+}
