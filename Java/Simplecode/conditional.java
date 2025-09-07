f4afbfc9ea4c9fef52577559f14862191b5ba747
Elakkiya <gelakkiya2000@gmail.com>
2025-09-07 09:57:28 +0530

    Create conditional.java

---
+++ Java/Simplecode/conditional.java
@@ -0,0 +1,21 @@
+package Java.Simplecode;
+
+public class conditional{
+public static void main(String args[]){
+    int mark = 80;
+
+    if (mark>=90){
+        System.out.println("Grade A");
+    }
+    else if (mark>=70){
+        System.out.println("grade C");
+    }
+    else if (mark>=60)
+    {
+        System.out.println("Grade D");
+    }
+    else{
+        System.out.println("Failed");
+    }
+}
+}
