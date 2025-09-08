f72de6ad9f5141c6ed45ece5ca346a908c80ede3
Elakkiya <gelakkiya2000@gmail.com>
2025-09-08 07:37:12 +0530

    Create Recursion.java

---
+++ Java/Simplecode/Recursion.java
@@ -0,0 +1,17 @@
+package Java.Simplecode;
+public class Recursion {
+    public static void main(String args[]){
+         int n;
+         natprint(n=10);
+
+    }
+    public static void natprint(int n){
+        if (n==1){
+            System.out.println(1);
+        }
+        else{
+            System.out.println(n);
+            natprint(n-1);
+        }
+    }
+}
