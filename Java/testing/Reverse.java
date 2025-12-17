2575bc794a7e6909fa444d4d996b1d1469946cbe
Elakkiya <gelakkiya2000@gmail.com>
2025-12-17 21:12:01 +0530

    Create Reverse.java

---
+++ Java/testing/Reverse.java
@@ -0,0 +1,11 @@
+public class Reverse {
+    public static void main(String[] args){
+        String s = "Elakkiya";
+        String rev = "";
+        for(int i=s.length()-1;i>=0;i--){
+            rev = rev + s.charAt(i);
+        }
+            System.out.println("Reversed String: " + rev)
+;        }
+    }
+    
